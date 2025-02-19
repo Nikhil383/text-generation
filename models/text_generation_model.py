ceedb25e9fcf0bbc0caaab2729300571d1a0e563
Nikhil383 <nikhilmahesh89@gmail.com>
2025-02-19 22:36:09 +0530

    seventh commit

--- models/text_generation_model.py
+++ models/text_generation_model.py
@@ -1,11 +1,10 @@
-import torch
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class TextGenerationModel:
     def __init__(self):
-        self.model_name = "t5-small"
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_name)
+        self.model_name = "NousResearch/DeepHermes-3-Llama-3-8B-Preview"
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name)
 
     def generate_text(self, prompt, max_length=100):
         inputs = self.tokenizer(prompt, return_tensors="pt")
